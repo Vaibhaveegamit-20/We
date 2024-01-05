94c42b442027311047d4181b24bdf14c6f5b752d
Vaibhaveegamit-20 <gamit.v@northeastern.edu>
2024-01-05 01:13:56 -0500

    added demo

---
+++ Demo.java
@@ -0,0 +1,14 @@
+public class Demo {
+    
+    public static void main(String args[]) 
+    {
+        int x = 6;
+        int y = 5;
+
+        //conditional operator
+        boolean result = x < y;
+
+        System.out.println(result);
+
+    }
+}
